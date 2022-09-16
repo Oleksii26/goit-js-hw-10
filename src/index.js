0da4b3670b6594f43ae67ce70aae3a4614ebc7d8
Oleksii26 <Kolesnik26@ukr.net>
2022-09-16 15:57:03 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
+import {fetchCountries} from '../fetchCountries'
+import debounce from 'lodash.debounce';
+
+const ref = {
+    input: document.querySelector('#search-box'),
+    list: document.querySelector('.country-list'),
+    info: document.querySelector('.country-info')
+}
 
 const DEBOUNCE_DELAY = 300;
+
+ref.input.addEventListener('input', debounce(onSearchCoutry, DEBOUNCE_DELAY))
+
+
+function onSearchCoutry(e) {
+    console.log(e.target.value)
+    ref.info.textContent = e.target.value.trim()
+}
